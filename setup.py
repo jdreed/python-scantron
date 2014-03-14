8b5d458970ec0faab93d42a64f4ffd514969afcb
Jonathan Reed <jdreed@mit.edu>
2014-03-14 13:28:45 -0400

    Add setup.py
    Add a setup.py script for the module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="PyScantron",
+    version="1.0",
+    description="Python bindings for Scantron readers",
+    author="Jonathan Reed",
+    author_email="jdreed@mit.edu",
+    license="GPL2",
+    py_modules=['scantron'],
+)
